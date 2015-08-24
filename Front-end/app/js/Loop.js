b702f449ca241d30b2243ea32c719832ee191207
Jean Carlo Rodriguez <alejandrojeancarlo@gmail.com>
2015-08-23 20:58:10 -0400

    File from WebStorm

--- Front-end/app/js/Loop.js
+++ Front-end/app/js/Loop.js
@@ -30,3 +30,77 @@ var isCapicua = function(num)
 }
 
 console.log('First capicua is: ', getFirstCapicua(50,100));
+/**
+ * funcion que devuelve los N primeros numeros pares
+ * @param limit (N numeros pares)
+ */
+var getOdd = function(limit)
+{
+    var i=0;
+    while(i<limit)
+    {
+        for(j=1;j<1000;j++) {
+            if (j % 2 == 0) {
+                console.log(j);
+                i++;
+            }
+            if(i==limit)
+            {
+                break;
+            }
+
+        }
+    }
+}
+/**
+ * funcion que devuelve los N primeros numeros impares
+ * @param limit (N numeros impares)
+ */
+var getEven = function(limit)
+{
+    var i=0;
+    while(i<limit)
+    {
+        for(j=1;j<1000;j++) {
+            if (j % 2 != 0) {
+                console.log(j);
+                i++;
+            }
+            if(i==limit)
+            {
+                break;
+            }
+
+        }
+    }
+}
+
+var getOddEven = function(limit)
+{
+    var i=0;
+    while(i<limit)
+    {
+        for(j=1;j<1000;j++) {
+
+            if(i==limit)
+            {
+                break;
+            }
+            if (j % 2 != 0) {
+                console.log('Even: ',j);
+                i++;
+                continue;
+            }
+            if (j % 2 == 0) {
+                console.log('Odd: ',j);
+                i++;
+                continue;
+            }
+            if(i==limit)
+            {
+                break;
+            }
+
+        }
+    }
+}
